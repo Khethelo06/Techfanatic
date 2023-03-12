357584ee5bd98fa5136ef625ba82cff105b505a1
BaNELISWa <101975300+BaNELISWa@users.noreply.github.com>
2023-03-12 11:41:30 +0000

    addded login

--- app.py
+++ app.py
@@ -3,12 +3,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home_page():
   return render_template("home.html")
 
-@app.route("/login")
-def login_page():
-  return render_template("login.html")
+@app.route("/studlogin/")
+def studlogin_page():
+  return render_template("studlogin.html")
 
+  app = Flask(__name__)
+  
+@app.route("/")
+def lecturelogin_page():
+  return render_template("lecturlogin.html")
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
+
+@app.route("/admilogin/")
+def admilogin_page():
+  return render_template("admilogin.html")
+  
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
+  
